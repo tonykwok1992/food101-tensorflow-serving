f119283ca866bc0c12307e704b80f14a4bb6265f
Tony Kwok <tonykwok1992@gmail.com>
2020-09-21 17:43:48 +0800

    simplify

--- flask-web-server/app.py
+++ flask-web-server/app.py
@@ -32,18 +32,9 @@ def image_classifier():
     logit = dict(zip(LARGE_CLASS_LIST, np.array(pred['predictions'][0])))
     return jsonify(sorted(logit.items(), key=operator.itemgetter(1), reverse = True))
 
-def img_to_array(img, data_format='channels_last', dtype='float32'):
-    if data_format not in {'channels_first', 'channels_last'}:
-        raise ValueError('Unknown data_format: %s' % data_format)
-    x = np.asarray(img, dtype=dtype)
-    if len(x.shape) == 3:
-        if data_format == 'channels_first':
-            x = x.transpose(2, 0, 1)
-    elif len(x.shape) == 2:
+def img_to_array(img):
+    x = np.asarray(img, dtype='float32')
+    if len(x.shape) == 2:
         if data_format == 'channels_first':
             x = x.reshape((1, x.shape[0], x.shape[1]))
-        else:
-            x = x.reshape((x.shape[0], x.shape[1], 1))
-    else:
-        raise ValueError('Unsupported image shape: %s' % (x.shape,))
     return x       
